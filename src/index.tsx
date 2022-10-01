5c99781e42804fa475114ec20395415d31269d73
Ashkan Tofangdar <ashkan4472@gmail.com>
2022-10-01 15:00:15 +0330

    Added Material UI

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import { CssBaseline } from '@mui/material';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,6 +12,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <CssBaseline />
       <App />
     </Provider>
   </React.StrictMode>
