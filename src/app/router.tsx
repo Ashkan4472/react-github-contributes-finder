0af9f116d32af7134ddf2a1bbc9fe6ea857d6aca
Ashkan Tofangdar <ashkan4472@gmail.com>
2022-10-02 06:12:01 +0330

    added react router

---
+++ src/app/router.tsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { Contributors } from '../pages/Contributors';
+import { Home } from '../pages/Home';
+
+export const router = createBrowserRouter([
+]);
